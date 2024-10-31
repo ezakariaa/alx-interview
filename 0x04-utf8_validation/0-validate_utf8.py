7b78d96d54c1ec3275c71f22cdee7e9fa29205a8
ezakariaa <zakariaorche@gmail.com>
2024-10-31 21:52:15 +0000

    0x04. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,38 +1,32 @@
+#!/usr/bin/python3
+"""
+0-validate_utf8.py
+"""
+
+
 def validUTF8(data):
-    # Number of bytes in the current UTF-8 character
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+    :param data: List of integers representing bytes
+    :return: True if data is a valid UTF-8 encoding, else False
+    """
     num_bytes = 0
 
-    # Masks to check the first byte of different UTF-8 characters
-    mask1 = 1 << 7  # 10000000
-    mask2 = 1 << 6  # 01000000
-
-    for byte in data:
-        # Keep only the last 8 bits of each integer
-        byte = byte & 0xFF
+    for num in data:
+        byte = num & 0xFF  # Only examine the least significant 8 bits
 
         if num_bytes == 0:
-            # Determine the number of bytes in the UTF-8 character
-            if (byte & mask1) == 0:
-                # 1-byte character
-                continue
-            elif (byte & (mask1 >> 1)) == mask1:
-                # Check if the byte starts with 110 for 2-byte characters
+            if byte >> 5 == 0b110:  # 110xxxxx
                 num_bytes = 1
-            elif (byte & (mask1 >> 2)) == (mask1 >> 1):
-                # Check if the byte starts with 1110 for 3-byte characters
+            elif byte >> 4 == 0b1110:  # 1110xxxx
                 num_bytes = 2
-            elif (byte & (mask1 >> 3)) == (mask1 >> 2):
-                # Check if the byte starts with 11110 for 4-byte characters
+            elif byte >> 3 == 0b11110:  # 11110xxx
                 num_bytes = 3
-            else:
-                # Invalid first byte
+            elif byte >> 7:  # 1xxxxxxx (must be 0xxxxxxx)
                 return False
         else:
-            # Check if this byte starts with 10xxxxxx
-            if not (byte & mask1 and not (byte & mask2)):
+            if byte >> 6 != 0b10:  # 10xxxxxx
                 return False
             num_bytes -= 1
 
-    # If num_bytes is not zero, we have an incomplete character at the end
     return num_bytes == 0
-
