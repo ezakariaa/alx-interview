be96b3be3e04dabe24a3a22efc3a9c653e4b4f95
ezakariaa <zakariaorche@gmail.com>
2024-12-12 15:52:35 -0800

    0x0A. Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,48 +1,48 @@
 #!/usr/bin/python3
-"""
-Module: Game of choosing Prime numbers
-"""
+"""Module for the Prime Game."""
 
 
 def isWinner(x, nums):
+    """Determines the winner of multiple prime number removal games.
+
+    Args:
+        x (int): Number of games to be played.
+        nums (list of int): List of upper limits for each game.
+
+    Returns:
+        str: The name of the winner, either "Ben" or "Maria".
     """
-    Determine who wins the most rounds of the Prime Game.
-    :param x: Number of rounds (integer).
-    :param nums: List of n values, one for each round.
-    :return: Name of the player with the most wins or None.
-    """
-    if not nums or x < 1:
+    if x <= 0 or nums is None:
         return None
-
-    # Step 1: Generate primes up to the maximum value in nums using Sieve of Eratosthenes
-    max_n = max(nums)
-    is_prime = [True] * (max_n + 1)
-    is_prime[0] = is_prime[1] = False  # 0 and 1 are not primes
-    for i in range(2, int(max_n**0.5) + 1):
-        if is_prime[i]:
-            for j in range(i * i, max_n + 1, i):
-                is_prime[j] = False
-
-    # Step 2: Precompute the number of primes up to each number
-    prime_count = [0] * (max_n + 1)
-    for i in range(1, max_n + 1):
-        prime_count[i] = prime_count[i - 1] + (1 if is_prime[i] else 0)
-
-    # Step 3: Determine the winner for each round
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if prime_count[n] % 2 == 1:  # Odd number of primes -> Maria wins
-            maria_wins += 1
-        else:  # Even number of primes -> Ben wins
-            ben_wins += 1
-
-    # Step 4: Return the overall winner
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
+    if x != len(nums):
+        return None
+    m_ben = 0
+    m_maria = 0
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        remove_all_multiples(a, i)
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            m_ben += 1
+        else:
+            m_maria += 1
+    if m_ben > m_maria:
         return "Ben"
-    else:
-        return None
+    if m_maria > m_ben:
+        return "Maria"
+    return None
 
+
+def remove_all_multiples(listers, x):
+    """Marks multiples of a prime number as non-prime in a list.
+
+    Args:
+        ls (list of int): List where multiples will be marked.
+        x (int): The prime number whose multiples are to be marked.
+    """
+    for index in range(2, len(listers)):
+        try:
+            listers[index * x] = 0
+        except (ValueError, IndexError):
+            break
